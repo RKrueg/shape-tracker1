d33ef700b7b25bc09341970023ad5786cba78f82
RKrueg <thegckid@gmail.com>
2022-12-12 19:50:50 -0800

    test should correctly determine whether three lengths are not a triangle, TEST PASSED

--- src/triangle.js
+++ src/triangle.js
@@ -1,5 +1,9 @@
-export default function Triangle(side1, side2, side3) {
+export default function Triangle(side1, side2, side3, height) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
 }
+
+Triangle.prototype.checkType = function() {
+  return "not a triangle";
+};
